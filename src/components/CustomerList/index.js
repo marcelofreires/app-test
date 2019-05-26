af094fd3acb405f1143d20c0b42b295f9b850a02
Marcelo Freire <marcelo.freire.silva@gmail.com>
2019-05-26 18:25:52 -0300

    Requisição de um customer e iniciar modal bio

--- src/components/CustomerList/index.js
+++ src/components/CustomerList/index.js
@@ -1,42 +1,76 @@
 import React, { Component } from 'react';
 
-import CustomerCard from '../../components/CustomerCard';
 import api from '../../services/api';
+import { FiXCircle } from "react-icons/fi";
+
+import CustomerCard from '../../components/CustomerCard';
+import CustomerBio from '../CustomerBio';
 
 import './styles.scss';
 
 class ClientList extends Component {
   state = {
     customers: [],
+    customer: {},
+    showModal: false
   }
 
   componentDidMount() {
-    this.showCustomer();
+    this.showCustomers();
   }
 
   handleDelete = async id => {
-    const response = await api.delete(`/customers/${id}`);
+    await api.delete(`/customers/${id}`);
 
-    console.log(response);
-    
-    this.showCustomer();
+    this.showCustomers();
   }
   
-  showCustomer = async () => {
+  showCustomers = async () => {
     const response = await api.get("/customers");
     
     this.setState({ customers: response.data.data});
   }
+  
+  showCustomer = async id => {
+    const response = await api.get(`/customers/${id}`);
+    console.log(response.data);
+    
+    this.setState({
+      customer: response.data,
+    });
+    
+    this.handleModal();
+  }
 
+  handleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  }
+  
   render() {
     return (
-      <ul className="customer-list">
-        {this.state.customers.map(customer => (
-          <li key={customer.id} className="customer-item">
-            <CustomerCard customer={customer} onClick={this.handleDelete} />
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul className="customer-list">
+          {this.state.customers.map(customer => (
+            <li key={customer.id} className="customer-item">
+              <CustomerCard customer={customer} onClickDelete={this.handleDelete} onClickMoreInfo={this.showCustomer} />
+            </li>
+          ))}
+        </ul>
+        
+        {this.state.showModal &&
+          <div className="modal">
+            <div className="modal-wrap">
+              <button
+                onClick={this.handleModal}
+                className="modal-close icon-button"
+              >
+                <FiXCircle size={28} />
+              </button>
+              <CustomerBio customer={this.state.customer} />
+            </div>
+          </div>
+        }
+      </>
     );
   }
 }
